f5bde70725f7adfec6a84d6e6c09fddb36cc7a9b
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-04-28 17:13:51 -0300

    create accounts app

---
+++ accounts/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from accounts.views import * 
+
+
+urlpatterns = [
+
+######## data, basic functions starts here
+		## url using for data saving into db
+		path('data-to-be-received/',SavingDataIntoDb.as_view(),name = "SavingDataIntoDb"),
+		##end here url using for data saving into db
+
+		## url using for data showing on frontend
+		path('data-to-be-show/',DashboardView.as_view(),name = "DashboardView"),
+		##end here url using for data showing on frontend
+
+
+######## data, basic functions ends here
+
+]
