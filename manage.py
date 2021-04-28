3639e292097a2a1668ad03aaf58857dab4b1dc72
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-04-28 17:13:12 -0300

    modify default module

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LHP_Dashboard.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vwap_offset.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
