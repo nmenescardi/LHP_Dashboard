f748cea35b2da0b9c209ececf39da96474348e6d
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-04-30 11:20:55 -0300

    calculate offset between price and vwap

--- vwap_offset/models/models.py
+++ vwap_offset/models/models.py
@@ -4,20 +4,33 @@ from django.contrib.auth.models import User
 
 class VwapData(models.Model):
 
-	''' Made a docstring for storing vwap offset dashboard data into this table '''
+    ''' Made a docstring for storing vwap offset dashboard data into this table '''
 
-	id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
-	open_dt = models.FloatField()
-	high = models.FloatField()
-	low = models.FloatField()
-	close = models.FloatField()
-	exchange = models.CharField(max_length = 256)
-	ticker = models.CharField(max_length = 256)
-	volume = models.FloatField()
-	price_time = models.DateTimeField(null = True, blank = True)
-	vwap = models.FloatField()
-	created_on = models.DateTimeField(auto_now_add = True)
-	updated_on = models.DateTimeField(auto_now = True)
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    open_dt = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+    close = models.FloatField()
+    exchange = models.CharField(max_length = 256)
+    ticker = models.CharField(max_length = 256)
+    volume = models.FloatField()
+    price_time = models.DateTimeField(null = True, blank = True)
+    vwap = models.FloatField()
+    created_on = models.DateTimeField(auto_now_add = True)
+    updated_on = models.DateTimeField(auto_now = True)
 
-	def __str__(self):
-		return ("instance of table-------> {} ".format(self.id))
+    def offset(self):
+        return self._get_percent_change( self.vwap, self.close )
+
+    def _get_percent_change(self, previous, current):
+        """ Helper method to get percentage of change between two numbers"""
+        if current == previous or previous == 0:
+            return 0
+
+        change = round((abs(current - previous) / previous) * 100.0, 2)
+        sign = "+" if current >= previous else "-"
+        return sign + str(change) + "%"
+
+
+    def __str__(self):
+        return ("instance of table-------> {} ".format(self.id))
