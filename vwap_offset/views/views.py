4d3a6e147321e2352796ce192f8c75d6b1d5bf2f
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-05-04 10:47:36 -0300

    query latest vwap value

--- vwap_offset/views/views.py
+++ vwap_offset/views/views.py
@@ -6,7 +6,7 @@ from vwap_offset.models import Pair
 from django.http.response import JsonResponse
 from django.utils import timezone
 from django.shortcuts import render,redirect
-from django.db.models.functions import Lower
+
 
 class SavingDataIntoDb(APIView):
 
@@ -53,26 +53,3 @@ class SavingDataIntoDb(APIView):
             context['message'] = 'An error occurred in saving data, please try again or contact us'
             context['status'] = 500
             return JsonResponse(context)
-
-
-
-""" class DashboardView(View):
-    context = {}
-    templates_name = 'data/data.html'
-
-    def get(self,request):
-
-        try:
-            all_data_list = Vwap.objects.all().order_by(Lower('price_time').desc())
-            ticker_list = []
-            all_data = []
-            for one_data in all_data_list:
-                if one_data.pair.ticker not in ticker_list:
-                    all_data.append(one_data)
-                    ticker_list.append(one_data.pair.ticker)
-
-            return render(request, self.templates_name, locals())
-        except Exception as e:
-            print(e)
-            return render(request, self.templates_name, locals())
- """
--- vwap_offset/viewsets.py
+++ vwap_offset/viewsets.py
@@ -1,9 +1,17 @@
 from rest_framework import viewsets
 from vwap_offset.models import Pair
 from vwap_offset.serializers import PairSerializer
+from django.db.models.functions import Lower
 
 class PairViewSet(viewsets.ModelViewSet):
     serializer_class = PairSerializer
 
     def get_queryset(self):
-        return Pair.objects.all()
+        all_pairs_data = Pair.objects.all().order_by(Lower('price_time').desc())
+        single_symbol_list = []
+        result = []
+        for one_data in all_pairs_data:
+            if one_data.symbol not in single_symbol_list:
+                result.append(one_data)
+                single_symbol_list.append(one_data.symbol)
+        return result
