c479c04cc8288e67e3c9fd7d22187ad85d5c9c6b
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-05-05 12:39:11 -0300

    fix url issue depending on environment

--- frontend/src/utils/services.js
+++ frontend/src/utils/services.js
@@ -1,4 +1,7 @@
-export const baseURL = 'http://localhost:8000/get';
+export const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://vwap-offset.herokuapp.com/get'
+    : 'http://localhost:8000/get';
 export const headers = {
   'Content-type': 'application/json',
 };
