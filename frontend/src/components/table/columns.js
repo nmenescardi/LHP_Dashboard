9859482cb5e11850cd443565fd98560f995a1007
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-05-08 08:46:06 -0300

    improve styles for vwap cell

--- frontend/src/components/table/columns.js
+++ frontend/src/components/table/columns.js
@@ -2,6 +2,22 @@ const roundCellValue = ({ cell: { value } }) => {
   return <>{value ? parseFloat(value).toFixed(3) : '...'}</>;
 };
 
+const bgColors = {
+  green: '#6bad10',
+  red: '#d0021b',
+};
+
+const vwap_styles = (bgColor = bgColors.red) => {
+  return {
+    background: bgColor,
+    color: '#fff',
+    padding: '6px 9px',
+    borderRadius: '7px',
+    fontSize: '15px',
+    letterSpacing: '0.6px',
+  };
+};
+
 export const COLUMNS = [
   {
     Header: 'Symbol',
@@ -12,7 +28,22 @@ export const COLUMNS = [
     accessor: 'offset',
     sortType: 'number',
     Cell: ({ cell: { value } }) => {
-      return <>{value ? (value > 0 ? '+' + value : value) + '%' : '...'}</>;
+      let vwap_value, styles;
+      if (value) {
+        if (value > 0) {
+          vwap_value = '+' + value;
+          styles = vwap_styles(bgColors.green);
+        } else {
+          vwap_value = value;
+          styles = vwap_styles();
+        }
+        vwap_value = vwap_value + '%';
+      } else {
+        vwap_value = '...';
+        styles = {};
+      }
+
+      return <span style={styles}>{vwap_value}</span>;
     },
   },
   {
