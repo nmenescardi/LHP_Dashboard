207bc4753f8714f8fb53bf5867916ca1c5b3f7ab
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-05-09 13:05:35 -0300

    refactor getPercentChange

--- frontend/src/components/pages/Homepage.js
+++ frontend/src/components/pages/Homepage.js
@@ -6,15 +6,7 @@ import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import replaceSymbol from './../../utils/replaceSymbol';
-
-const getPercentChange = (previous, current) => {
-  previous = parseFloat(previous);
-  current = parseFloat(current);
-  if (current === previous || !previous) return 0;
-
-  const change = ((Math.abs(current - previous) / previous) * 100.0).toFixed(2);
-  return current >= previous ? change : change * -1;
-};
+import getPercentChange from './../../utils/getPercentChange';
 
 export default () => {
   const [pairs, setPairs] = useState([]);
