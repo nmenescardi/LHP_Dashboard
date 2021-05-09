12bc8d2166fc39876c1cea538d739f9519caf2aa
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-05-09 14:22:51 -0300

    refactor table styles

--- frontend/src/components/table/Table.js
+++ frontend/src/components/table/Table.js
@@ -1,6 +1,7 @@
 import React, { useMemo, useState } from 'react';
 import { useTable, useSortBy, useFilters } from 'react-table';
 import { COLUMNS } from './columns';
+import tableStyles from './tableStyles';
 import MaUTable from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -9,50 +10,10 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
 import InputBase from '@material-ui/core/InputBase';
-import { fade, makeStyles } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
-
-const useStyles = makeStyles((theme) => ({
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
-    },
-  },
-}));
 
 export const Table = ({ data }) => {
   const columns = useMemo(() => COLUMNS, []);
-  //const data = useMemo(() => data, [])
 
   const {
     getTableProps,
@@ -72,7 +33,7 @@ export const Table = ({ data }) => {
     useSortBy
   );
 
-  const classes = useStyles();
+  const classes = tableStyles();
 
   const [filterInput, setFilterInput] = useState('');
 
