17f5d3b95ca9ff03837b220bfc5bc47d08ed7ce0
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-05-11 14:05:37 -0300

    WIP separate state

--- frontend/src/utils/handleWebSocket.js
+++ frontend/src/utils/handleWebSocket.js
@@ -2,7 +2,7 @@ import replaceSymbol from './replaceSymbol';
 import getPercentChange from './getPercentChange';
 import { wssURL } from './services';
 
-const handleWebSocket = (connection, pairs, setPairs) => {
+const handleWebSocket = (connection, setPairsPrice) => {
   if (
     !(connection && connection.current && connection.current.readyState === 1)
   ) {
@@ -10,21 +10,14 @@ const handleWebSocket = (connection, pairs, setPairs) => {
     connection.current.onmessage = (e) => {
       const results = JSON.parse(e.data);
 
-      if (!results || !pairs.length) return;
+      if (!results) return;
 
-      setPairs(
-        pairs.map((pair) => {
-          let result = results.find(
-            (result) => replaceSymbol(pair.symbol) === replaceSymbol(result.s)
-          );
-
-          return result
-            ? {
-                ...pair,
-                price: result.p,
-                offset: getPercentChange(pair.vwap, result.p),
-              }
-            : pair;
+      setPairsPrice(
+        results.map((result) => {
+          return {
+            symbol: replaceSymbol(result.s),
+            price: result.p,
+          };
         })
       );
     };
