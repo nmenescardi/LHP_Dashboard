1ab9d6371d4b5c43a0904ef5d73cd13d0e2ebe62
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-05-12 11:29:48 -0300

    remove comment

--- LHP_Dashboard/settings.py
+++ LHP_Dashboard/settings.py
@@ -97,14 +97,7 @@ DATABASES = {
         'HOST': env.str('MYSQL_DB_HOST'),
     }
 }
-""" 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-} 
-"""
+
 
 
 # Password validation
